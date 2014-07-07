751e959abcbee54947cf9928692e991d91957e40
Jonathan (Jonny) Johannes <jonny@jonnyjohannes.com>
2014-07-06 23:55:42 -0600

    javascript in own file

--- app/assets/javascripts/decks.js
+++ app/assets/javascripts/decks.js
@@ -1,29 +1,50 @@
-var decks = (function($, window, document, undefined) {
+$(document).ready(function() {
+  $('#query-form').submit(function(event) {
+    
+    // Stop form from submitting normally
+    event.preventDefault();
 
-  var init = function() {
-    initSearch();
-  };
-
-  var initSearch = function() {
-    $('#query-form').submit(function(event) {
-      
-      // Stop form from submitting normally
-      event.preventDefault();
-
-      // Get some values from elements on the page:
-      var $form = $( this ),
-        term = $form.find( "input[name='query']" ).val(),
-        url = $form.attr( "action" );
-     
-      // Send the data using post
-      var posting = $.ajax({
-        type: 'POST',
-        url: url,
-        data: {query: term}, 
-        dataType: 'script'
-      });
-      
+    // Get some values from elements on the page:
+    var $form = $( this ),
+      term = $form.find( 'input[name="query"]' ).val(),
+      url = $form.attr( 'action' );
+   
+    // Send the data using post
+    var posting = $.ajax({
+      type: 'POST',
+      url: url,
+      data: {query: term}, 
+      dataType: 'script'
+    }).done(function() {
+      initDragDrop();
     });
-  };
+    
+  });
 
-})(jQuery, window, document);
+  function initDragDrop() {
+    $('.thumb-draggable').draggable({
+      containment: 'html',
+      helper: 'clone'
+    });
+    $('#deck1-droppable').droppable({
+      drop: function(event, ui) {
+        setDeck($(this), ui);
+      }
+    });
+    $('#deck2-droppable').droppable({
+      drop: function(event, ui) {
+        setDeck($(this), ui);
+      }
+    });
+  }
+
+  function setDeck(dropArea, ui) {
+    var draggableVideo = ui.draggable;
+    dropArea.html(draggableVideo.children('.embed_html').text());
+    $('iframe').each(function(index, elem) {
+      elem.setAttribute('width','100%');
+    });
+  }
+
+});
+
